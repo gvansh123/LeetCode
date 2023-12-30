bfd020638b75e759f79344d93ed236cf966a18cb
gvansh123 <131516007+gvansh123@users.noreply.github.com>
2023-12-30 19:18:01 +0530

    Time: 7 ms (82.29%) | Memory: 56.3 MB (96.14%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        int [] arr = new int [100001];
+        for(int x : costs)arr[x]++;
+        int count = 0;
+        int index = 1;
+        while(index<100001 && coins>=index*arr[index]){
+            coins-=index*arr[index];
+            count+=arr[index];
+            index++;
+        }
+
+       while(index<100001 && coins>=index){
+            coins-=index;
+            count++;
+        }
+        return count;
+    }
+}
