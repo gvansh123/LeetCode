eb8855129a30d5ff9fc1c757001b7d10dbd4ab98
gvansh123 <131516007+gvansh123@users.noreply.github.com>
2024-02-21 19:40:31 +0530

    Time: 2 ms (88.97%) | Memory: 48 MB (70.02%) - LeetSync

---
+++ 143-reorder-list/reorder-list.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) 
+    { 
+         if(head == null || head.next == null)
+         return ;
+
+        ListNode mid = getMid(head);
+        ListNode hSecond = reverse(mid);
+        ListNode hFirst = head;
+
+
+        while(hFirst != null && hSecond != null)
+        {
+            ListNode temp = hFirst.next;
+            hFirst.next = hSecond;
+            hFirst = temp;
+            temp = hSecond.next;
+            hSecond.next = hFirst; 
+            hSecond = temp; 
+              
+        }
+        if(hFirst != null)
+        hFirst.next = null;
+    }
+    public ListNode getMid(ListNode node)
+    {
+        ListNode fast = node;
+        ListNode slow = node;
+        while(fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    public ListNode reverse(ListNode mid)
+    {
+        ListNode prev = null;
+        ListNode present = mid;
+        ListNode then = present.next;
+        while(present != null)
+        {
+            present.next = prev;
+            prev = present;
+            present = then;
+            if(then != null)
+            then = then.next;
+
+        }
+        return prev;
+    }
+}
