23a435bb4a14b31d007e3951212739ab60ca2715
gvansh123 <131516007+gvansh123@users.noreply.github.com>
2024-05-25 14:26:41 +0530

    Time: 249 ms (7.22%) | Memory: 46.5 MB (5.35%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static boolean vow(char c)
+    public static boolean vowel(char c)
     {
         return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
     }
@@ -7,13 +7,13 @@ class Solution {
         Stack<Character> l=new Stack<>();
         for(char c :s.toCharArray())
         {
-            if(vow(c))
+            if(vowel(c))
               l.push(c);
         }
         String ans="";
         for(char c :s.toCharArray())
         {
-            if(vow(c))
+            if(vowel(c))
             {
               ans+=String.valueOf(l.peek());
               l.pop();
