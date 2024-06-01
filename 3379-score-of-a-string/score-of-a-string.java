5e147a54e066c640d5135122868b6b2fecbc0f72
gvansh123 <131516007+gvansh123@users.noreply.github.com>
2024-06-01 21:59:42 +0530

    Time: 1 ms (99.87%) | Memory: 41.6 MB (94.65%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int ans=0;
+        for(int i=1;i<s.length();i++)
+        {
+            ans+=Math.abs((int)s.charAt(i)-(int)s.charAt(i-1));
+        }
+        return ans;
+    }
+}
