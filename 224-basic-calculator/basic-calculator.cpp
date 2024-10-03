d63c69d7c6dee188149ddf3b9619073425aa9f3e
gvansh123 <131516007+gvansh123@users.noreply.github.com>
2024-10-03 22:29:27 +0530

    Time: 10 ms (47.01%) | Memory: 9.6 MB (66.92%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,41 +1,24 @@
 class Solution {
-public:
-    int calculate(string s) {
-        stack<int> number;
-        stack<int> operators;
-        int num = 0;
-        int ans = 0;
-        int sign = 1;
-        for (char c : s) 
-        { 
-            if (isdigit(c)) 
-            {
-                num = num * 10 + (c - '0');
-            }
-            else 
-            {
-                ans += sign * num;
-                num = 0;
-                if (c == '+') 
-                    sign = 1;
-                if (c == '-') 
-                    sign = -1;
-                if (c == '(') 
-                {
-                    number.push(ans);
-                    operators.push(sign);
-                    ans = 0;
-                    sign = 1;
-                }
-                if (c == ')' && operators.size()) 
-                {
-                    ans = operators.top() * ans + number.top();
-                    operators.pop(); 
-                    number.pop();
-                }
-            }
-        }
+ public:
+  int calculate(string s) {
+    int ans = 0;
+    int num = 0;
+    int sign = 1;
+    stack<int> st;
+    st.push(sign);
+    for (char c : s)
+      if (isdigit(c))
+        num = num * 10 + (c - '0');
+      else if (c == '(')
+        st.push(sign);
+      else if (c == ')')
+        st.pop();
+      else if (c == '+' || c == '-') {
         ans += sign * num;
-        return ans;
-    }
+        sign = (c == '+' ? 1 : -1) * st.top();
+        num = 0;
+      }
+
+    return ans + sign * num;
+  }
 };
